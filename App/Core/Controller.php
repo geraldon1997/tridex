32830f0734a52351a0d74d4942a22331aadbda5c
geraldon1997 <geraldon1997@gmail.com>
2021-01-15 07:10:39 +0100

    corrected login issue

--- App/Core/Controller.php
+++ App/Core/Controller.php
@@ -59,49 +59,42 @@ class Controller
 
     public function view($view, $params = null)
     {
-        $this->views['nonauth'] = array_combine(range(1, count($this->views['nonauth'])), $this->views['nonauth']);
-        $this->views['auth']['general'] = array_combine(range(1, count($this->views['auth']['general'])), $this->views['auth']['general']);
-        $this->views['auth']['admin'] = array_combine(range(1, count($this->views['auth']['admin'])), $this->views['auth']['admin']);
-        $this->views['auth']['moderator'] = array_combine(range(1, count($this->views['auth']['moderator'])), $this->views['auth']['moderator']);
-        
         if (!isset($_SESSION['email'])) {
-            $exist_in_nonauth = array_search($view, $this->views['nonauth']);
-            if (!$exist_in_nonauth) {
-                $exists_in_general = array_search($view, $this->views['auth']['general']);
-                if (!$exists_in_general) {
-                    Response::code(404);
-                    return '<h1>page not found</h1>';
-                }
-                Response::code(403);
-                return Response::redirect('/user/signin');
-            }
-            Response::code(200);
-            return View::renderView($this->main, $view, $params);
-        }
+            $non = in_array($view, $this->views['nonauth']);
 
-        $userid = User::findSingle(User::$table, 'email', $_SESSION['email'])[0]['id'];
-        $is_logged_in = Auth::findSingle(Auth::$table, 'user_id', $userid)[0]['is_logged_in'];
+            if ($non) {
+                return View::renderView('main', $view, $params);
+            }
+
+            return View::renderView('main', 'signin', $params);
+        }
         
-        $exists_in_general = array_search($view, $this->views['auth']['general']);
-        if (!$exists_in_general) {
-            $roleid = User::findSingle(User::$table, 'email', $_SESSION['email'])[0]['role_id'];
-            $role = Role::findSingle(Role::$table, 'id', $roleid)[0]['role'];
+        $general = in_array($view, $this->views['auth']['general']);
+        $layout = 'dashboard';
 
-            $exists_in_secret = array_search($view, $this->views['auth'][$role]);
-            if (!$exists_in_secret) {
-                Response::code(404);
-                return Response::redirect('/user/signin');
+        if ($general) {
+            if ($view === 'login') {
+                return View::renderView('main', $view, $params);
             }
-            Response::code(200);
-            if (!$is_logged_in) {
-                return View::renderView($this->main, $view, $params);
+            return View::renderView($layout, $view, $params);
+        }
+
+        if (User::isAdmin()) {
+            $admin = in_array($view, $this->views['auth']['admin']);
+
+            if ($admin) {
+                return View::renderView($layout, $view, $params);
             }
-            return View::renderView($this->dashboard, $view, $params);
         }
-        Response::code(200);
-        if (!$is_logged_in) {
-            return View::renderView($this->main, $view, $params);
+
+        if (User::isModerator()) {
+            $moderator = in_array($view, $this->views['auth']['moderator']);
+
+            if ($moderator) {
+                return View::renderView($layout, $view, $params);
+            }
         }
-        return View::renderView($this->dashboard, $view, $params);
+        session_unset();
+        return View::renderView('main', 'signin', $params);
     }
 }
